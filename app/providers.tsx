cf67d35eb78ee3cf29a814be037c802b98d66635
goalandpriority-ui <goalandpriority@gmail.com>
2026-01-20 18:50:06 +0530

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,19 +3,15 @@
 import { WagmiProvider, createConfig, http } from 'wagmi'
 import { base } from 'wagmi/chains'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { injected } from 'wagmi/connectors'
 import { ReactNode } from 'react'
 
 const queryClient = new QueryClient()
 
 const config = createConfig({
   chains: [base],
-  connectors: [
-    injected()
-  ],
   transports: {
-    [base.id]: http()
-  }
+    [base.id]: http(),
+  },
 })
 
 export default function Providers({ children }: { children: ReactNode }) {
