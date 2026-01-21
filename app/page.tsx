52982337d9e78b06a50d2dc557f41bfe4e8944fe
goalandpriority-ui <goalandpriority@gmail.com>
2026-01-21 22:39:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
 'use client';
 
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
 
 export default function Home() {
   const { address, isConnected } = useAccount();
-  const { connect, connectors } = useConnect();
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
   const { disconnect } = useDisconnect();
 
   return (
     <main style={{ padding: 20 }}>
-      <h1>Whale Check</h1>
+      <h1>🐋 Whale Check</h1>
 
       {!isConnected && (
-        <button onClick={() => connect({ connector: connectors[0] })}>
+        <button onClick={() => connect()}>
           Connect Wallet
         </button>
       )}
