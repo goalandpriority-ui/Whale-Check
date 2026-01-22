f4324c9b197902d734af8ca044b89316c70d84b5
goalandpriority-ui <goalandpriority@gmail.com>
2026-01-22 22:50:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,23 @@
 
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 
-export default function Page() {
+export default function Home() {
   const { address, isConnected } = useAccount()
   const { connect, connectors } = useConnect()
   const { disconnect } = useDisconnect()
 
-  if (isConnected) {
-    return (
-      <div>
-        <p>Connected:</p>
-        <b>{address}</b>
-        <br />
-        <button onClick={() => disconnect()}>Disconnect</button>
-      </div>
-    )
-  }
-
   return (
-    <div>
-      {connectors.map((connector) => (
-        <button
-          key={connector.uid}
-          onClick={() => connect({ connector })}
-        >
+    <div style={{ padding: 20 }}>
+      {!isConnected ? (
+        <button onClick={() => connect({ connector: connectors[0] })}>
           Connect Wallet
         </button>
-      ))}
+      ) : (
+        <>
+          <p>Connected: {address}</p>
+          <button onClick={() => disconnect()}>Disconnect</button>
+        </>
+      )}
     </div>
   )
 }
