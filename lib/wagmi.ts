a8b27d3692b50d0c337a4a8124fb1608a5639c10
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-09 15:54:11 +0530

    Update wagmi.ts

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,13 +1,13 @@
-import { http, createConfig } from 'wagmi'
+import { createConfig, http } from 'wagmi'
 import { base } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 
 export const config = createConfig({
   chains: [base],
   connectors: [
-    injected()
+    injected(),
   ],
   transports: {
-    [base.id]: http()
-  }
+    [base.id]: http(),
+  },
 })
