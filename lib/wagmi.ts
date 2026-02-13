6a506a41629aef053fa9c1efb761d79dbbb9ed02
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-13 13:17:03 +0530

    Remove injected connector from wagmi config

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -2,13 +2,11 @@
 
 import { createConfig, http } from 'wagmi'
 import { base } from 'wagmi/chains'
-import { injected } from 'wagmi/connectors'
 import { walletConnect } from '@wagmi/connectors'
 
 export const config = createConfig({
   chains: [base],
   connectors: [
-    injected(),
     walletConnect({
       projectId: 'c56357101a152b811310071d8366d90'
     }),
