96bdc8b1787b0378c06c045de3c61dabfd66789d
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-14 16:49:48 +0530

    Update alchemy.ts

--- lib/alchemy.ts
+++ lib/alchemy.ts
@@ -1,20 +1,8 @@
-import axios from 'axios'
+import { Alchemy, Network } from "alchemy-sdk";
 
-const ALCHEMY_URL = process.env.ALCHEMY_URL!
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_RPC?.split("/v2/")[1],
+  network: Network.ETH_MAINNET, // Base network uses Ethereum mainnet RPC
+};
 
-export async function getTokenTransfers(address: string) {
-  const response = await axios.post(ALCHEMY_URL, {
-    jsonrpc: "2.0",
-    id: 1,
-    method: "alchemy_getAssetTransfers",
-    params: [{
-      fromBlock: "0x0",
-      toBlock: "latest",
-      fromAddress: address,
-      category: ["erc20"],
-      withMetadata: true
-    }]
-  })
-
-  return response.data.result.transfers
-}
+export const alchemy = new Alchemy(config);
