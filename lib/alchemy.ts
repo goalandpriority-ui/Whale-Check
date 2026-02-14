c34d30d62b1b8071f37c992529a6f3b5f2cdee93
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-14 17:03:05 +0530

    Update alchemy.ts

--- lib/alchemy.ts
+++ lib/alchemy.ts
@@ -2,7 +2,7 @@ import { Alchemy, Network } from "alchemy-sdk";
 
 const config = {
   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_RPC?.split("/v2/")[1],
-  network: Network.ETH_MAINNET, // Base network uses Ethereum mainnet RPC
+  network: Network.ETH_MAINNET
 };
 
 export const alchemy = new Alchemy(config);
