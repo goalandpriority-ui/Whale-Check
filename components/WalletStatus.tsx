c1f887447588fba1050f9e0a7ea8f9902fd1bf2f
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-16 07:14:16 +0530

    Update WalletStatus.tsx

--- components/WalletStatus.tsx
+++ components/WalletStatus.tsx
@@ -1,5 +1,43 @@
 "use client"
 
+import { useAccount, useConnect, useDisconnect } from "wagmi"
+
+export default function WalletStatus() {
+  const { address, isConnected } = useAccount()
+  const { connectAsync, connectors } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  if (isConnected) {
+    return (
+      <div>
+        <p>Connected: {address}</p>
+        <button onClick={() => disconnect()}>
+          Disconnect
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          key={connector.id}
+          onClick={async () => {
+            try {
+              await connectAsync({ connector })
+            } catch (error) {
+              console.error(error)
+            }
+          }}
+        >
+          Connect {connector.name}
+        </button>
+      ))}
+    </div>
+  )
+}"use client"
+
 import { ReactNode } from "react"
 import { WagmiConfig, configureChains, createConfig } from "wagmi"
 import { mainnet } from "wagmi/chains"
